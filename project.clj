753ce649def1337763f9d93484d0ef898e9f1b4e
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-05-19 16:26:10 +0300

    libs versions are updated

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.6.0"]
-                 [compojure "1.1.6"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.2"]
+                 [compojure "1.1.8"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [ring/ring-json "0.3.1"]]
