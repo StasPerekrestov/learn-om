e6c695b6d88dfd6b96599ad45198e2b152ea9b5b
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-06-14 15:23:51 +0300

    clojurescript is updated

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/clojurescript "0.0-2234"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [om "0.6.4"]
                  [compojure "1.1.8"]
