513f3fa70e374c02fcbfa828690281659f25dcc6
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-08-30 22:43:38 +0300

    core.async is updated

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [com.facebook/react "0.11.1"]
                  [om "0.7.1"]
