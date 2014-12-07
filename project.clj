35df3ebf675ee00ef016ac3c45858cbd66048333
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-12-07 21:28:32 +0200

    deps

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.facebook/react "0.11.2"]
-                 [om "0.8.0-alpha1"]
+                 [om "0.8.0-beta2"]
                  [compojure "1.2.1"]
-                 [figwheel "0.1.5-SNAPSHOT"]
+                 [figwheel "0.1.6-SNAPSHOT"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [ring/ring-json "0.3.1"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [lein-ring "0.8.13"]
-            [lein-figwheel "0.1.5-SNAPSHOT"]]
+            [lein-figwheel "0.1.6-SNAPSHOT"]]
   :ring {:handler myproject.handler/app
          :init myproject.handler/init
          :destroy myproject.handler/destroy
