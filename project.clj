1ea85ae174e41a9b2f1f6ed8254572e083c83195
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-12-17 22:18:11 +0200

    deps are updated

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2411"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.facebook/react "0.11.2"]
-                 [om "0.8.0-beta2"]
-                 [compojure "1.2.1"]
-                 [figwheel "0.1.6-SNAPSHOT"]
+                 [om "0.8.0-beta3"]
+                 [compojure "1.3.1"]
+                 [figwheel "0.1.7-SNAPSHOT"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [ring/ring-json "0.3.1"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [lein-ring "0.8.13"]
-            [lein-figwheel "0.1.6-SNAPSHOT"]]
+            [lein-figwheel "0.1.7-SNAPSHOT"]]
   :ring {:handler myproject.handler/app
          :init myproject.handler/init
          :destroy myproject.handler/destroy
